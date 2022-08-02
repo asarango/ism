06079fdbd3331ed897098e33782986898ac20816
desarrollo <desarrollo@ism.edu.ec>
2022-08-02 12:40:21 -0500

    arreglo

--- backend/views/pep-detalle/_enfoques.php
+++ backend/views/pep-detalle/_enfoques.php
@@ -1,61 +1,78 @@
 <div class="row">
-    <div class="col-lg-3 col-md-3">
-        
 
-    </div>
-    <div class="col-lg-6 col-md-6">
-        <p class="text-primero"><b><u><i class="fas fa-cogs"> Enfoques de aprendizaje</i></u></b></p>
-        <?php
-        foreach ($registros as $reg) {
-            if ($reg->tipo == 'enfoques_aprendizaje') {
-                $reg->contenido_opcion ? $check = 'checked' : $check = '';
-                ?>
+
+    <p class="text-primero"><b><u><i class="fas fa-cogs"> Enfoques de aprendizaje</i></u></b></p>
+
+    <table class="table table-condensed table-bordered">
+        <thead>
+            <tr>
+                <th>Acción</th>
+                <th>Principal</th>
+                <th>Secundaria</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php
+            foreach ($registros as $reg) {
+                if ($reg->tipo == 'enfoques_aprendizaje') {
+                    $reg->contenido_opcion ? $check = 'checked' : $check = '';
+                    ?>
+            <tr>
+                <td>
                     <div class="form-check form-switch">
                         <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" <?= $check ?>
-                        onclick="update(<?= $reg->id ?>)">
-                        <label class="form-check-label" for=""><?= $reg->contenido_texto ?></label>
-                      </div>
+                           onclick="update(<?= $reg->id ?>)">
+                    
+                    </div>
+                </td>
+                <td><label class="form-check-label" for=""><?= $reg->referencia ?></label></td>
+                <td><label class="form-check-label" for=""><?= $reg->contenido_texto ?></label></td>
+                
+            </tr>   
                 <?php
             }
         }
         ?>
-        
-    </div>
+        </tbody>
+    </table>
+
+
+
+
     <!--fin de conceptos relacionados-->
-    
-    
+
+
     <!--inicio de atributos de perfil-->
-    <div class="col-lg-3 col-md-3">
-        
-    </div>
+
     <!--fin de atributos de perfil-->
-    
+
 </div>
 
 
 <script>
-    function update(id){
+    function update(id) {
         var url = '<?= yii\helpers\Url::to(['update-selection']) ?>';
 
         params = {
-          id : id      
+            id: id
         };
 
         $.ajax({
-                data: params,
-                url: url,
-                type: 'POST',
-                beforeSend: function () {},
-                success: function (resp) {   
-                    respuesta = JSON.parse(resp);
-                    var estado = respuesta.status;
+            data: params,
+            url: url,
+            type: 'POST',
+            beforeSend: function () {},
+            success: function (resp) {
+                respuesta = JSON.parse(resp);
+                var estado = respuesta.status;
 
-                    if(estado == 'ok'){
-                        //alert('Actualizado correctamente!');
-                    }else{
-                        alert('El registro no se actualizó!');
-                    }
+                if (estado == 'ok') {
+                    //alert('Actualizado correctamente!');
+                } else {
+                    alert('El registro no se actualizó!');
                 }
-            });
+            }
+        });
     }
 </script>

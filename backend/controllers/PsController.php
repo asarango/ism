3d7d369c90ce9eacf49d2212f2a9d96d60874cb6
desarrollo <desarrollo@ism.edu.ec>
2022-08-04 15:49:42 -0500

    plan semanal

--- backend/controllers/PsController.php
+++ backend/controllers/PsController.php
@@ -59,7 +59,73 @@ class PsController extends Controller {
     }
     
     public function actionIndex1(){
-        echo 'desde controller';
+        
+        $usuarioLog = Yii::$app->user->identity->usuario;
+        
+        /** Para tomar la fecha desde **/
+        if(isset($_GET['desde'])){
+            $desde = $_GET['desde'];            
+        }else{
+            $desde = $this->calcula_fecha_desde();
+        }
+                
+        $hasta = date("Y-m-d", strtotime($desde."+ 4 days")); /** fecha hasta **/ 
+        
+        /** Para tomar el calendario de la semana **/
+        $helper = new \backend\models\helpers\CalendarioSemanal($desde, $hasta, $usuarioLog); 
+        $calendario = $helper->fechas;
+        /*******************************/
+        
+        $actividades = $this->get_actividades_semanal($desde, $hasta, $usuarioLog); /** Actividades de la semana del docente **/                
+        
+        return $this->render('index',[
+            'calendario' => $calendario,
+            'actividades' => $actividades
+        ]);                
+    }
+    
+//    private function buscar_totales($arrayActividades, $fecha){
+//        $key = array_search($fecha, array_column($arrayActividades, 'inicio'));
+//        if($key){
+//            return $arrayActividades[$key]['total_actividades'];
+//        }else{
+//            return 0;
+//        }        
+//    } 
+    
+    
+    
+    private function calcula_fecha_desde(){
+        $hoy = date('Y-m-d');
+        $hoyNumero = date('N');
+        
+        if ($hoyNumero == 1){
+            return $hoy;
+        }else{
+            $totalRestar = $hoyNumero - 1;
+            return date("Y-m-d", strtotime($hoy."- $totalRestar days"));
+        }
+    }   
+    
+    private function get_actividades_semanal($desde, $hasta, $usuario){
+        $con = Yii::$app->db;
+        $query = "select substring(cast(ac.inicio as varchar),0,11) as inicio 
+		,count(ac.id) as total_actividades
+                from	scholaris_actividad ac
+                                inner join scholaris_clase cl on cl.id = ac.paralelo_id
+                                inner join op_faculty f on f.id = cl.idprofesor 
+                                inner join res_users u on u.partner_id = f.partner_id
+                where 	ac.inicio between '$desde' and '$hasta'
+                                and u.login = '$usuario'
+                group by ac.inicio;";
+        $res = $con->createCommand($query)->queryAll();
+        return $res;
+    }
+    
+    
+    
+    public function actionDetalle(){
+        print_r($_GET);
     }
 
 }

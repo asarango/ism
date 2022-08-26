35ad4dda233ee976d8d74e0cd933b23a626653eb
asisdesarrollo <asisdesarrollo@ism.edu.ec>
2022-08-26 21:02:17 +0000

    modificacion dece y casos

--- backend/views/dece-casos/create.php
+++ backend/views/dece-casos/create.php
@@ -24,18 +24,22 @@ $ahora = date('Y-m-d H:i:s');
         <div class="card shadow col-lg-7 col-md-7">
             <div class=" row align-items-center p-2">
                 <div class="col-lg-1">
-                    <h3><img src="ISM/main/images/submenu/curriculum-vitae-y-cv.png" width="64px" class="img-thumbnail"></h3>
+                    <img src="ISM/main/images/submenu/firma-electronica.png" width="" class="img-thumbnail">                    
                 </div>
 
                 <div class="col-lg-11">
                     <h3>CREACIÓN - CASOS</h3>
+                    <?php $nombreEstudiante = $model->estudiante->last_name .' '.$model->estudiante->middle_name . ' ' . $model->estudiante->first_name ?>                           
+                    <h6><b>Estudiante: </b><span style="color:red"><?=$nombreEstudiante?></span></h6>
+                    <h6><b>Nuevo Caso: </b><span style="color:red"><?=$model->numero_caso?></span></h6>
                 </div>
               
                 <!-- FIN DE CABECERA -->
+
                 <?=
                             Html::a(
-                                '<span class="badge rounded-pill" style="background-color:#ff9e18"><i class="fa fa-briefcase" aria-hidden="true"></i> Dece - Casos</span>',
-                                ['dece-casos/historico','id'=>0],
+                                '<span class="badge rounded-pill" style="background-color:#9e28b5"><i class="fa fa-briefcase" aria-hidden="true"></i> Dece - Casos</span>',
+                                ['dece-casos/historico','id'=>$model->id_estudiante],
                                 ['class' => 'link']
                             );
                             ?>
